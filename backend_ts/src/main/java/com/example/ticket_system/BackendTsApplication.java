8234d126734daf52dfe5f099a4e3e93c277d6337
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2021-09-27 09:18:10 -0300

    criando o projeto

---
+++ backend_ts/src/main/java/com/example/ticket_system/BackendTsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTsApplication.class, args);
+	}
+
+}

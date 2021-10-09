0f999ffb1886b59cfbe1505151e81d5463c0f2f0
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2021-10-09 10:29:39 -0300

    adicionando inputs

--- backend/src/main/java/com/example/ticket_system/BackendTsApplication.java
+++ backend/src/main/java/com/example/ticket_system/BackendTsApplication.java
@@ -41,19 +41,25 @@ public class BackendTsApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception{
 
-		Empresa e1 = new Empresa(1, "nome_emp", "razao_emp", "12345678901234", "email_emp", "endereco_emp", "1234567890");
-		Empresa e2 = new Empresa(2, "2nome_emp", "2razao_emp", "22345678901234", "2email_emp", "2endereco_emp", "21234567890");
+		Empresa e1 = new Empresa(1, "Amazon", "Amazon", "12345678901234", "amazon@hotmail.com", "Rua 22, Setor Antena, Numero 127", "61 99867634");
+		Empresa e2 = new Empresa(2, "Facebook", "Facebook", "12345678901235", "facebook@hotmail.com", "Rua 20, Setor Sul, Numero 721", "61 99667678");
+		Empresa e3 = new Empresa(3, "Amazon", "Amazon", "12345678901237", "amazon2@hotmail.com", "Rua 22, Setor Antena, Numero 127", "61 99837634");
+		Empresa e4 = new Empresa(4, "Facebook", "Facebook", "12345678901238", "facebook2@hotmail.com", "Rua 20, Setor Sul, Numero 721", "61 99267678");
 
-		Funcionario f1 = new Funcionario(1, "nome_func", "cargo_func", "email_func", "1234567990");
-		Funcionario f2 = new Funcionario(2, "2nome_func", "2cargo_func", "2email_func", "21234567890");
+		Funcionario f1 = new Funcionario(1, "João Marcelo", "Desenvolvedor", "joaomarcelo@hormail.com", "61 99667620");
+		Funcionario f2 = new Funcionario(2, "Gabriel", "Desenvoldedor", "gabriel@hotmail.com", "61 99667630");
+		Funcionario f3 = new Funcionario(3, "João Marcelo", "Desenvolvedor", "joaomarcelo2@hormail.com", "61 99667320");
+		Funcionario f4 = new Funcionario(4, "Gabriel", "Desenvoldedor", "gabriel2@hotmail.com", "61 99668630");
 
-		Recado r1 = new Recado(1,  "option1", "option5","setor", "mensagem", e1, f1);
-		Recado r2 = new Recado(2,  "option2", "option3","2setor", "2mensagem", e2, f2);
+		Recado r1 = new Recado(1,  "option1", "option5","Programação", "Planejar a próxima aplicação da empresa.", e1, f1);
+		Recado r2 = new Recado(2,  "option2", "option3","Programação", "Testar a próxima aplicação da empresa.", e2, f2);
+		Recado r3 = new Recado(3,  "option2", "option4","Programação", "Planejar a próxima aplicação da empresa.", e3, f3);
+		Recado r4 = new Recado(4,  "option1", "option4","Programação", "Testar a próxima aplicação da empresa.", e4, f4);
 
 
-		empresaDAO.saveAll(Arrays.asList(e1,e2));
-		funcionarioDAO.saveAll(Arrays.asList(f1,f2));
-		recadoDAO.saveAll(Arrays.asList(r1,r2));
+		empresaDAO.saveAll(Arrays.asList(e1,e2,e3,e4));
+		funcionarioDAO.saveAll(Arrays.asList(f1,f2,f3,f4));
+		recadoDAO.saveAll(Arrays.asList(r1,r2,r3,r4));
 
 
 	}

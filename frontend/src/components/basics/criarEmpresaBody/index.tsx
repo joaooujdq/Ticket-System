235b804fd8e7c069f03a21785733a32ec5a8c675
João Marcelo <49964202+joaooujdq@users.noreply.github.com>
2021-10-31 19:38:10 -0300

    melhorando a pagina html de cadastro de empresa

--- frontend/src/components/basics/criarEmpresaBody/index.tsx
+++ frontend/src/components/basics/criarEmpresaBody/index.tsx
@@ -55,19 +55,19 @@ const CriarEmpresaBody: React.FC = () => {
         <>
 
             <body id='CriarEmpresaBody'>
-
+            <h2 id='TitleBar'>Cadastro de Empresa</h2>
                 <ul id='CriarEmpresaUl'>
-                    <div id='divH1'>
-                        
+                    
+                <div id='CriarEmpresaForm'>
+                <div id='divH1'>
                 <h1>Nome: </h1> 
                 <h1>Razão Social: </h1> 
                 <h1>CNPJ: </h1>
                 <h1>Email: </h1>
                 <h1>Endereço: </h1>
                 <h1>Telefone: </h1>
-               
-
                 </div>
+
                 <div id='divInput'>
                 <input type="text" value={inputNomeEmp} onChange={e => setInputNomeEmp(e.target.value)} />  
                 <input type="text" value={inputRazaoEmp} onChange={e => setInputRazaoEmp(e.target.value)} />
@@ -77,11 +77,13 @@ const CriarEmpresaBody: React.FC = () => {
                 <input type="text" value={inputTelefoneEmp} onChange={e => setInputTelefoneEmp(e.target.value)} />
                     
                 </div>
+                </div>
                 
+                <button onClick={postMsg}>Cadastrar</button>
             </ul>
            
                 
-            <button onClick={postMsg}>Cadastrar</button>
+            
           
 
 
